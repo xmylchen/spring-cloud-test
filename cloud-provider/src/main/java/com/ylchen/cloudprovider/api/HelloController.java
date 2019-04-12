548f32de79911d5e3ce22010b084dda305b26fb8
chenyl <chenyl@datatowisdom.cn>
2019-04-12 13:58:25 +0800

    first commit

---
+++ cloud-provider/src/main/java/com/ylchen/cloudprovider/api/HelloController.java
@@ -0,0 +1,26 @@
+package com.ylchen.cloudprovider.api;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+@RestController
+public class HelloController {
+    private final Logger logger = Logger.getLogger(getClass().toString());
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/hi")
+    public String home(@RequestParam(value = "name", defaultValue = "forezp") String name) {
+        return "hi " + name + " ,i am from port:" + port;
+    }
+}
