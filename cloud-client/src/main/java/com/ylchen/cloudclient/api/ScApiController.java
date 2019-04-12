8dfa740c76bcc0f57113cbadc85f92cc98ee8c17
chenyl <chenyl@datatowisdom.cn>
2019-04-12 16:15:01 +0800

    zuul

--- cloud-client/src/main/java/com/ylchen/cloudclient/api/ScApiController.java
+++ cloud-client/src/main/java/com/ylchen/cloudclient/api/ScApiController.java
@@ -2,11 +2,14 @@ package com.ylchen.cloudclient.api;
 
 import com.ylchen.cloudclient.service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api-a/data")
-public class HelloController {
+@RequestMapping("/api-b/data")
+public class ScApiController {
     @Autowired
     public HelloService helloService;
     @RequestMapping(value = "/hi", method = RequestMethod.POST)
